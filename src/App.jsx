09f26f33c3c8cae7e8a6493e3be97dd98eabfaef
Conci <concibntt@gmail.com>
2024-03-28 00:16:36 -0300

    lista

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <NavBar/>
-      <ItemListContainer greetings="Trabajamos para usted"/>
+      <ItemListContainer greetings="Trabajamos para vos"/>
     </>
   )
 }
